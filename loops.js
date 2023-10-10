8dd649f1e6268f5c33538abd68d9de079a100283
theacalaya@gmail.com <81848569+Acalaya@users.noreply.github.com>
2023-10-10 16:40:34 +0545

    Loops

---
+++ loops.js
@@ -0,0 +1,51 @@
+//for loop
+for (let i = 0; i<=2; i++) {
+    console.log("Darling");
+};
+
+//while loop
+let j = 0;
+while (j < 3) {
+    console.log("M");
+    j++;
+};
+//loops not just for printing but function(), logic.., data...
+ 
+//do while loop
+let k = 0;
+do {
+    console.log("O");
+    k++;
+} while (k < 3);
+ // while checks the condition first whether do while does the work and then check the condition 
+
+ //arrays in loops
+const plants = ["Aloevera", "Snake"];
+for(let a = 0; a<plants.length; a++) {
+    console.log(plants[a]);
+}
+
+//for loops variance in javascript
+//for each loop (only for array, forEach is array function)
+plants.forEach(function(v,i) {
+    console.log(v);
+}); 
+
+//for in loop
+
+//for up loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
