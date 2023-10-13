a1b4ecba6d3f381617ca483f89e2e9941d4ccf4f
theacalaya@gmail.com <81848569+Acalaya@users.noreply.github.com>
2023-10-13 15:26:06 +0545

    function

---
+++ function.js
@@ -0,0 +1,23 @@
+function add() {
+    let c = 1;
+    let d = 2;
+    console.log(c+d);
+};
+add();
+// function helps to separate tasks and reuse codes.
+
+function add(a,b)/*parameter */ {
+    console.log(a+b);
+    return true; // javascript is dynamic type language, so no need to put specific value 
+};
+add(1,2); //argument value // the same function is now dynamic
+
+//function call, and argument pass
+
+
+
+
+
+
+
+
